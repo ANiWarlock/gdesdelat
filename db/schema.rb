5da0dab745e49e4f1bf9a52f9090ac2bd768b858
Zuf AniWarlock Karimov <AniWarlock@gmail.com>
2013-09-24 16:11:44 +0000

    gallery fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924024508) do
+ActiveRecord::Schema.define(version: 20130924051624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20130924024508) do
     t.string   "contact_person2"
     t.string   "metro"
     t.string   "schedule"
+    t.string   "experience"
+    t.integer  "state"
   end
 
   create_table "csasses", force: true do |t|
